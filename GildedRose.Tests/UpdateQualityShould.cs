d7abfbd33d721ac62eadcc5729de3b0486a734a1
Gerald Ncube <gerald.ncube@se.com>
2022-05-31 17:22:49 +0100

    fixed the test

--- GildedRose.Tests/UpdateQualityShould.cs
+++ GildedRose.Tests/UpdateQualityShould.cs
@@ -11,5 +11,20 @@ namespace GildedRose.Tests
 
             statusUpdater.UpdateQuality(new List<Item>());
         }
+
+        [Fact]
+        public void LowerQuality_GivenNormalItem()
+        {
+            var statusUpdater = new InventoryStatusUpdater();
+
+
+            var normalItem = new Item { Name = "Normal Normal", Quality = 20, SellIn = 10 };
+            var expectedQuality = normalItem.Quality - 1;
+            var items = new List<Item> { normalItem };
+            statusUpdater.UpdateQuality(items);
+
+            Assert.Equal(expectedQuality, normalItem.Quality);
+
+        }
     }
 }
