da5b824f803a2ac5b3b963452476e63269ab1fc6
Gerald Ncube <gerald.ncube@se.com>
2022-05-31 19:56:48 +0100

    Completed the tests

---
+++ GildedRose.Tests/InventoryStatusUpdaterTests/GivenConjuredItem_UpdateQualityShould.cs
@@ -0,0 +1,69 @@
+﻿using GildedRose.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Tests.InventoryStatusUpdaterTests
+{
+    public class GivenConjuredItem_UpdateQualityShould
+    {
+        private readonly InventoryStatusUpdater _statusUpdater;
+
+        public GivenConjuredItem_UpdateQualityShould()
+        {
+            _statusUpdater = new InventoryStatusUpdater();
+        }
+
+        [Fact]
+        public void ReduceQualityByTwo()
+        {
+            var conjuredItem = new Item { Name = "Conjured Sword", Quality = 20, SellIn = 10 };
+            var items = new List<Item> { conjuredItem };
+            var expectedResult = 18;
+
+            _statusUpdater.UpdateQuality(items);
+
+            Assert.Equal(expectedResult, conjuredItem.Quality);
+        }
+
+        [Fact]
+        public void ReduceQualityByFour_AfterSellInReachesZero()
+        {
+            var conjuredItem = new Item { Name = "Conjured Sword", Quality = 20, SellIn = 1 };
+            var items = new List<Item> { conjuredItem };
+            var expectedResult = 10;
+
+            _statusUpdater.UpdateQuality(items); // -2
+            _statusUpdater.UpdateQuality(items); // -4
+            _statusUpdater.UpdateQuality(items); // -4
+
+            Assert.Equal(expectedResult, conjuredItem.Quality);
+        }
+
+        [Fact]
+        public void FloorQualityAtZero_GivenSellInRemains()
+        {
+            var conjuredItem = new Item { Name = "Conjured Sword", Quality = 1, SellIn = 10 };
+            var items = new List<Item> { conjuredItem };
+            var expectedResult = 0;
+
+            _statusUpdater.UpdateQuality(items); // -2
+
+            Assert.Equal(expectedResult, conjuredItem.Quality);
+        }
+
+        [Fact]
+        public void FloorQualityAtZero_GivenSellInZero()
+        {
+            var conjuredItem = new Item { Name = "Conjured Sword", Quality = 3, SellIn = 0 };
+            var items = new List<Item> { conjuredItem };
+            var expectedResult = 0;
+
+            _statusUpdater.UpdateQuality(items); // -2
+
+            Assert.Equal(expectedResult, conjuredItem.Quality);
+        }
+    }
+}
--- GildedRose/InventoryStatusUpdater.cs
+++ GildedRose/InventoryStatusUpdater.cs
@@ -6,6 +6,8 @@
         {
             for (var i = 0; i < Items.Count; i++)
             {
+
+
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
@@ -13,6 +15,10 @@
                         if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                         {
                             Items[i].Quality = Items[i].Quality - 1;
+                            if (Items[i].Name.StartsWith("Conjured") && Items[i].Quality > 0)
+                            {
+                                Items[i].Quality -= 1;
+                            }
                         }
                     }
                 }
@@ -59,6 +65,10 @@
                                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                                 {
                                     Items[i].Quality = Items[i].Quality - 1;
+                                    if (Items[i].Name.StartsWith("Conjured") && Items[i].Quality > 0)
+                                    {
+                                        Items[i].Quality -= 1;
+                                    }
                                 }
                             }
                         }
