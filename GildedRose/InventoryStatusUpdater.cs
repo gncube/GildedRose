51b29bdeebac1eb7631f283fbc04f8467b8b9015
Gerald Ncube <gerald.ncube@se.com>
2022-06-01 10:04:32 +0100

    move files into simple classes

--- GildedRose/InventoryStatusUpdater.cs
+++ GildedRose/InventoryStatusUpdater.cs
@@ -2,18 +2,94 @@
 {
     public class InventoryStatusUpdater
     {
+        private const string BackstagePassItemName = "Backstage passes to a TAFKAL80ETC concert";
+        private const string AgedBrieItemName = "Aged Brie";
+        private const string SulfurasItemName = "Sulfuras, Hand of Ragnaros";
+        private const string ConjuredItemPrefix = "Conjured";
+
         public void UpdateQuality(IList<Item> items)
         {
             foreach (Item item in items)
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                UpdateItemQuality(item);
+
+                UpdateSellIn(item);
+            }
+        }
+
+        private static void UpdateItemQuality(Item item)
+        {
+            if (item.Name != AgedBrieItemName && item.Name != BackstagePassItemName)
+            {
+                if (item.Quality > 0)
+                {
+                    if (item.Name != SulfurasItemName)
+                    {
+                        item.Quality--;
+                        if (item.Name.StartsWith(ConjuredItemPrefix) && item.Quality > 0)
+                        {
+                            item.Quality -= 1;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality++;
+
+                    if (item.Name == BackstagePassItemName)
+                    {
+                        if (item.SellIn < 11)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality++;
+                            }
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void UpdateSellIn(Item item)
+        {
+            if (item.Name != SulfurasItemName)
+            {
+                item.SellIn--;
+            }
+            HandleIfExpired(item);
+        }
+
+        private static void HandleIfExpired(Item item)
+        {
+            if (item.SellIn < 0)
+            {
+                HandleExpired(item);
+            }
+        }
+
+        private static void HandleExpired(Item item)
+        {
+            if (item.Name != AgedBrieItemName)
+            {
+                if (item.Name != BackstagePassItemName)
                 {
                     if (item.Quality > 0)
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        if (item.Name != SulfurasItemName)
                         {
-                            item.Quality = item.Quality - 1;
-                            if (item.Name.StartsWith("Conjured") && item.Quality > 0)
+                            item.Quality--;
+                            if (item.Name.StartsWith(ConjuredItemPrefix) && item.Quality > 0)
                             {
                                 item.Quality -= 1;
                             }
@@ -22,69 +98,16 @@
                 }
                 else
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-                        }
-                    }
+                    item.Quality -= item.Quality;
                 }
-
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
+            }
+            else
+            {
+                if (item.Quality < 50)
                 {
-                    item.SellIn = item.SellIn - 1;
-                }
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != "Aged Brie")
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality = item.Quality - 1;
-                                    if (item.Name.StartsWith("Conjured") && item.Quality > 0)
-                                    {
-                                        item.Quality -= 1;
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
+                    item.Quality++;
                 }
             }
         }
     }
-
 }
--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -1,47 +1,38 @@
 ﻿namespace GildedRose.Console
 {
-    internal class Program
+    public class Program
     {
-        private static IList<Item> Items;
+        public static IList<Item> items;
 
         private static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            Items = new List<Item>
-            {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          };
+            items = new List<Item> { new Item { Name = "+5 Dexterity Vest",SellIn = 10,Quality = 20 },
+new Item { Name = "Aged Brie",
+SellIn = 2,
+Quality = 0 },
+new Item { Name = "Elixir of the Mongoose",
+SellIn = 5,
+Quality = 7 },
+new Item { Name = "Sulfuras, Hand of Ragnaros",
+SellIn = 0,
+Quality = 80 },
+new Item { Name = "Backstage passes to a TAFKAL80ETC concert",
+SellIn = 15,
+Quality = 20 },
+new Item { Name = "Conjured Mana Cake",
+SellIn = 3,
+Quality = 6 } };
 
             var app = new InventoryStatusUpdater();
 
-            app.UpdateQuality(Items);
+            app.UpdateQuality(items);
 
             System.Console.ReadKey();
 
         }
 
-
-
-    }
-
-    public class Item
-    {
-        public string Name { get; set; }
-
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
     }
 
 }
